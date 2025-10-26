07ac21f08fafba7607c14f716bdb499fa54e5b72
Smartonlabs Inc. <110349587+LibertasIoT@users.noreply.github.com>
2025-10-26 14:26:38 -0400

    Auto commit VirtualIrrigationControl V20251026.164615.875.12.

---
+++ LibertasTutorial/VirtualIrrigationControl.53.lua
@@ -0,0 +1,95 @@
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+local ____exports = {}
+local DefaultTimeoutMilli = 10 * 1000
+local function TurnOffValve(state)
+    if state[3] then
+        state[3] = false
+        local timer = state[4]
+        if timer ~= nil then
+            Libertas_TimerCancel(timer)
+            state[4] = nil
+        end
+        Libertas_VirtualDeviceAttributesChanged(state[2], {{6, {0, 16385}}})
+    end
+end
+local function TurnOnValve(state, onTime)
+    for ____, curValve in ipairs(state[1]) do
+        if curValve[2] ~= state[2] then
+            TurnOffValve(curValve)
+        end
+    end
+    state[3] = true
+    local curTimer = state[4]
+    local expire = os.msticks() + onTime
+    if curTimer == nil then
+        state[4] = Libertas_TimerNew(
+            onTime,
+            function(timer, tag)
+                local state = tag
+                TurnOffValve(state)
+            end,
+            state
+        )
+    else
+        Libertas_TimerUpdate(curTimer, onTime)
+    end
+    state[5] = expire
+    Libertas_VirtualDeviceAttributesChanged(state[2], {{6, {0, 16385}}})
+end
+local function VirtualIrrigationController(valves)
+    local allValues = {}
+    for ____, valve in ipairs(valves) do
+        local curState = {allValues, valve, false}
+        allValues[#allValues + 1] = curState
+        Libertas_SetOnVirtualDevice(
+            valve,
+            function(device, ref, action, data, tag)
+                local state = tag
+                if action == 2 or action == 3 then
+                    local rpt = {}
+                    for ____, clusterReq in ipairs(data) do
+                        local rptAttributes = {}
+                        local rptStatus = {}
+                        local cluster, attributes = table.unpack(clusterReq, 1, 2)
+                        for ____, attr in ipairs(attributes) do
+                            if attr == 0 then
+                                rptAttributes[attr] = state[3]
+                            elseif attr == 16385 then
+                                local curTimer = state[4]
+                                if curTimer == nil then
+                                    rptAttributes[attr] = 0
+                                else
+                                    local expire = state[5]
+                                    local milliLeft = expire - os.msticks()
+                                    rptAttributes[attr] = milliLeft <= 0 and 0 or math.floor(milliLeft / 100 + 0.5)
+                                end
+                            else
+                                rptStatus[attr] = 134
+                            end
+                        end
+                        rpt[#rpt + 1] = {cluster, rptAttributes, rptStatus}
+                    end
+                    Libertas_VirtualDeviceAttributesRsp(device, ref, rpt)
+                elseif action == 8 then
+                    local cluster, id, cmdBody = table.unpack(data, 1, 3)
+                    local curTimer = curState[3]
+                    if id == 1 then
+                        TurnOnValve(state, DefaultTimeoutMilli)
+                    elseif id == 0 then
+                        TurnOffValve(state)
+                    elseif id == 66 then
+                        local onTime = cmdBody[1]
+                        TurnOnValve(state, onTime * 100)
+                    end
+                else
+                    Libertas_VirtualDeviceStatusRsp(device, ref, 128)
+                end
+            end,
+            curState
+        )
+        Libertas_VirtualDeviceAttributesChanged(valve, {{6, {0, 16385}}})
+    end
+    Libertas_WaitReactive()
+end
+____exports.VirtualIrrigationController = VirtualIrrigationController
+return ____exports
---
+++ LibertasTutorial/VirtualIrrigationControl.ts
@@ -0,0 +1,95 @@
+declare type ValveState = [allValves: ValveState[], valve: LibertasDevice, onOff: boolean, timer?: LibertasTimer, expire?:number]
+const DefaultTimeoutMilli = 10 * 1000
+
+function TurnOffValve(state: ValveState) {
+    if (state[2]) {
+        state[2] = false;
+        const timer = state[3]
+        if (timer != undefined) {
+            Libertas_TimerCancel(timer)
+            state[3] = undefined
+        }
+        Libertas_VirtualDeviceAttributesChanged(state[1], [
+            [Matter.Clusters.OnOff, [Matter.Attributes.OnOff.OnOff, Matter.Attributes.OnOff.OnTime]]
+        ])
+    }
+}
+
+function TurnOnValve(state: ValveState, onTime: number) {
+    for (const curValve of state[0]) {
+        if (curValve[1] != state[1]) {
+            TurnOffValve(curValve)
+        }
+    }
+    state[2] = true
+    const curTimer = state[3]
+    const expire = os.msticks() + onTime
+    if (curTimer == undefined) {
+        state[3] = Libertas_TimerNew(onTime, (timer, tag)=>{
+            const state = tag as ValveState
+            TurnOffValve(state)
+        }, state)
+    } else {
+        Libertas_TimerUpdate(curTimer, onTime)
+    }
+    state[4] = expire
+    Libertas_VirtualDeviceAttributesChanged(state[1], [
+        [Matter.Clusters.OnOff, [Matter.Attributes.OnOff.OnOff, Matter.Attributes.OnOff.OnTime]]
+    ])
+}
+
+function VirtualIrrigationController(valves: LibertasVirtualDevice[]) {
+    const allValues: ValveState[] = []
+    for (const valve of valves) {
+        const curState : ValveState = [allValues, valve, false]
+        allValues.push(curState)
+        Libertas_SetOnVirtualDevice(valve, (device, ref, action, data, tag)=>{
+            const state = tag as ValveState
+            if (action == LibertasDeviceAction.ReadRequest || action == LibertasDeviceAction.SubscribeRequest) {
+                const rpt: LibertasClusterReport[] = []
+                for (const clusterReq of data as LibertasClusterReadReq[]) {
+                    const rptAttributes: LibertasAttributes = {}
+                    const rptStatus: LibertasIdStatus = {}
+                    const [cluster, attributes] = clusterReq;
+                    for (const attr of attributes) {
+                        if (attr == Matter.Attributes.OnOff.OnOff) {
+                            rptAttributes[attr] = state[2]
+                        } else if (attr == Matter.Attributes.OnOff.OnTime) {
+                            const curTimer = state[3]
+                            if (curTimer == undefined) {
+                                rptAttributes[attr] = 0
+                            } else {
+                                const expire = state[4]!
+                                const milliLeft = expire - os.msticks()
+                                rptAttributes[attr] = (milliLeft <= 0) ? 0 : Math.round(milliLeft / 100)
+                            }
+                        } else {
+                            rptStatus[attr] = Matter.Status.UnsupportedAttribute
+                        }
+                    }
+                    rpt.push([cluster, rptAttributes, rptStatus])
+                }
+                Libertas_VirtualDeviceAttributesRsp(device, ref, rpt)
+            } else if (action == LibertasDeviceAction.InvokeCommandRequest) {
+                const [cluster, id, cmdBody] = data as LibertasCommand
+                const curTimer = curState[2]
+                if (id == Matter.Commands.OnOff.On) {
+                    TurnOnValve(state, DefaultTimeoutMilli)
+                } else if (id == Matter.Commands.OnOff.Off) {
+                    TurnOffValve(state)
+                } else if (id == Matter.Commands.OnOff.OnWithTimedOff) {
+                    const onTime = cmdBody![Matter.Fields.OnOff.OnWithTimedOff.OnTime] as number
+                    TurnOnValve(state, onTime * 100)
+                }
+            } else {
+                Libertas_VirtualDeviceStatusRsp(device, ref, Matter.Status.InvalidAction)
+            }
+        }, curState);
+        Libertas_VirtualDeviceAttributesChanged(valve, [
+            [Matter.Clusters.OnOff, [Matter.Attributes.OnOff.OnOff, Matter.Attributes.OnOff.OnTime]]
+        ])
+    }
+    Libertas_WaitReactive();
+}
+
+export {VirtualIrrigationController}
